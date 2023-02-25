790d6c9c6aacc63a5a82bf4ffeabfe9fa851e9e6
yesminete <yesmine.lajmi@gmail.com>
2023-02-26 00:15:54 +0100

    code

--- src/pages/student/FurniturePost.js
+++ src/pages/student/FurniturePost.js
@@ -1,5 +1,15 @@
+import Post from "components/student/Post";
+import DetailsFurniture from "components/student/DetailsFurniture";
+import House from "house.jpg";
+import pic from "avatar.avif";
+
 const FurniturePost = () => {
-  return <div>Welcome to Furniture Post</div>;
+  return (
+    /*zid top bar ili fiha logo fou9 post*/
+    <div>
+    <Post Details={DetailsFurniture} condition={false} postimage={House} avatarimage={pic}/>
+    </div>
+  )
 };
 
 export default FurniturePost;
